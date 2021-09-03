1f32247f0aba430937db2da04ba93f2ceb1867be
Tejas <tejassinghsital@gmail.com>
2021-09-03 20:30:10 +0530

    Arranged edureka 30 question directopry and question files

---
+++ Star Pattern Practice/edruka 30 questions/src/Q1PyramidProgram.java
@@ -0,0 +1,32 @@
+/*
+1. Pyramid Program
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * *
+
+*/
+
+public class Q1PyramidProgram
+{
+	public static void main(String[] args)
+	{
+		int n=5;
+
+		for(int i=1; i<=5;i++)
+		{
+
+			for(int j=i; j<=n;j++)
+			{
+				System.out.print(" ");
+			}
+
+			for(int j=1; j<=i;j++)
+			{
+				System.out.print(" "+"*");
+			}
+		System.out.println();
+		}
+	}
+}
---
+++ Star Pattern Practice/edruka 30 questions/src/Q2RightTriangleStarPattern.java
@@ -0,0 +1,24 @@
+/*
+2. Right Triangle Star Pattern
+*
+* *
+* * *
+* * * *
+* * * * *
+
+*/
+
+public class Q2RightTriangleStarPattern
+{
+	public static void main(String[] args)
+	{
+		int n=5;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				
+			}
+		}
+	}
+}
